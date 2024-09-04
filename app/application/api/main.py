1a81c93a840d1a1f562444f6c3816e8a58a53235
AlexanderLukash <sashass20062727@gmail.com>
2024-09-04 18:02:01 +0300

    Created base logger.

--- app/application/api/main.py
+++ app/application/api/main.py
@@ -1,7 +1,10 @@
+import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+import betterlogging as bl
 
 from app.application.api.v1.urls import router as v1_router
 from app.bot.main import (
@@ -12,6 +15,32 @@ from app.settings.config import load_config
 
 
 config = load_config("././.env")
+
+
+def setup_logging():
+    """Set up logging configuration for the application.
+
+    This method initializes the logging configuration for the application.
+    It sets the log level to INFO and configures a basic colorized log for
+    output. The log format includes the filename, line number, log level,
+    timestamp, logger name, and log message.
+
+    Returns:
+        None
+
+    Example usage:
+        setup_logging()
+
+    """
+    log_level = logging.INFO
+    bl.basic_colorized_config(level=log_level)
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s",
+    )
+    logger = logging.getLogger(__name__)
+    logger.info("Starting bot")
 
 
 @asynccontextmanager
@@ -43,4 +72,5 @@ def create_app():
         allow_headers=["*"],
     )
     app.include_router(v1_router, prefix="/api")
+    setup_logging()
     return app
